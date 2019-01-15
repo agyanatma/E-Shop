a01f91c27fd23db8007c80037d422dff96a5a731
Agyan Atma <bitblast77@gmail.com>
2019-01-15 10:32:07 +0700

    databased

--- e_shop/app/product_images.php
+++ e_shop/app/product_images.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class user extends Model
+class product_images extends Model
 {
     //
 }
---
+++ e_shop/database/migrations/2019_01_15_031430_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('order_date');
+            $table->string('user_id');
+            $table->string('product_id');
+            $table->decimal('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
