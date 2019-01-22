0bbd3e3e8b4eb65e503072cc32792db6354edd72
faisalrizkyrahadian <44174161+faisalrizkyrahadian@users.noreply.github.com>
2019-01-22 15:21:51 +0700

    "update

--- e_shop/app/Http/Controllers/Frontend/PagesController.php
+++ e_shop/app/Http/Controllers/Frontend/PagesController.php
@@ -9,61 +9,61 @@ class PagesController extends Controller
     public function index(){
         $title = 'Welcome To Laravel!';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.index')->with ('title', $title);
+        return view ('pages.frontend.index')->with ('title', $title);
     }
     public function indexuser(){
         $title = 'array';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.indexuser')->with ('title', $title);
+        return view ('pages.frontend.indexuser')->with ('title', $title);
     }
     public function category(){
         $title = 'Kategori';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.category')->with ('title', $title);
+        return view ('pages.frontend.category')->with ('title', $title);
     }
     public function order(){
         $title = 'ORDER';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.order')->with ('title', $title);
+        return view ('pages.frontend.order')->with ('title', $title);
     }
     public function product(){
         $title = 'PRODUK';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.product')->with ('title', $title);
+        return view ('pages.frontend.product')->with ('title', $title);
     }
     public function user(){
         $title = 'USER';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.user')->with ('title', $title);
+        return view ('pages.frontend.user')->with ('title', $title);
     }
     public function shop(){
         $title = 'SHOP';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.shop')->with ('title', $title);
+        return view ('pages.frontend.shop')->with ('title', $title);
     }
     public function tambahproduct(){
         $title = 'TAMBAH PRODUCT';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.tambahproduct')->with ('title', $title);
+        return view ('pages.frontend.tambahproduct')->with ('title', $title);
     }
     public function registeraccount(){
-        $title = 'TAMBAH PRODUCT';
+        $title = 'Register Akun';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.registeraccount')->with ('title', $title);
+        return view ('pages.frontend.registeraccount')->with ('title', $title);
     }
     public function loginaccount(){
         $title = 'TAMBAH PRODUCT';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.loginaccount')->with ('title', $title);
+        return view ('pages.frontend.loginaccount')->with ('title', $title);
     }
     public function wishlist(){
         $title = 'TAMBAH PRODUCT';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.wishlist')->with ('title', $title);
+        return view ('pages.frontend.wishlist')->with ('title', $title);
     }
     public function editproduct(){
         $title = 'Edit Produk';
         //return view ('pages.index', compact ('title'));
-        return view ('pages.editproduct')->with ('title', $title);
+        return view ('pages.frontend.editproduct')->with ('title', $title);
     }
 }   
