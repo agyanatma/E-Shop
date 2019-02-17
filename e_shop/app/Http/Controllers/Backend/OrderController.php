1593126aebcadeeeb8fe9159d13a935fee283a5d
Agyan Atma <bitblast77@gmail.com>
2019-02-17 23:55:06 +0700

    fixed

--- e_shop/app/Http/Controllers/Backend/OrderController.php
+++ e_shop/app/Http/Controllers/Backend/OrderController.php
@@ -19,8 +19,8 @@ class OrderController extends Controller
     public function show($id){
         $orders = Orders::with(['orderDetail','orderDetail.product'])->find($id);
         $details = $orders->orderDetail;
-        $item = Order_product::with(['product','buyer'])->where('user_id',Auth::id())->get();
-        dd($item->toArray());
+        //$item = Order_product::with(['product','buyer'])->where('user_id',Auth::id())->get();
+        //dd($item->toArray());
         return view('pages.admin.view_order')->with('orders', $orders)->with('details', $details);
     }
 
